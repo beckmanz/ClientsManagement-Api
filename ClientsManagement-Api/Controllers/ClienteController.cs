5fdc40eb01f9f495ee280eb25048363398c9e39f
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-06-20 14:31:41 -0300

    feat: cria ClienteController e Endpoints
    - Criada ClienteController
    - Criado Endpoint POST RegistrarNovoCliente
    - Criado Endpoint GET ListarClientes

---
+++ ClientsManagement-Api/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+using ClientsManagement_Api.Models.Dtos;
+using ClientsManagement_Api.Models.Entity;
+using ClientsManagement_Api.Services.Cliente;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientsManagement_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteInterface _clienteInterface;
+
+        public ClienteController(IClienteInterface clienteInterface)
+        {
+            _clienteInterface = clienteInterface;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseModel<ClienteModel>>> RegistrarNovoCliente(NovoClienteDto ClienteDto)
+        {
+            var response = await _clienteInterface.RegistrarNovoCliente(ClienteDto);
+            return response;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseModel<List<ClienteModel>>>> ListarClientes()
+        {
+            var response = await _clienteInterface.ListarClientes();
+            return response;
+        }
+    }
+}
